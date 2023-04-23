a0a11931a2f50738c86c51d3fddfbe737c2f3a3b
张嘉嘉123 <m202271824@hust.edu.cn>
2023-04-23 13:51:07 +0000

    update etmem/src/etmemd_src/etmemd_project.c.
    Signed-off-by: 张嘉嘉123 <m202271824@hust.edu.cn>

--- etmem/src/etmemd_src/etmemd_project.c
+++ etmem/src/etmemd_src/etmemd_project.c
@@ -155,9 +155,8 @@ static enum opt_result engine_of_group(GKeyFile *config, const char *group_name,
     }
 
     *eng = get_eng_by_name(proj, eng_name);
-    if (eng_name){
-        free(eng_name);
-    }       
+    if (eng_name)
+        free(eng_name);      
     return OPT_SUCCESS;
 }
 
